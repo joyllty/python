b05662c1c6b6e9edb2c5d93b41eb337ccf11a311
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-10-15 17:16:02 -0300

    Create ex005.py

---
+++ exercíciosGuanabara/ex005.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor
+num = int(input("Digite um número inteiro: "))
+#ant = num - 1
+#suc = num + 1
+print(f'O antecessor do número {num} é {num-1} e o sucessor desse número é {num+1}')
