46d0438356b800d9a4b50a9fd8425afce5be3774
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-10-15 17:34:05 -0300

    Update ex006.py

--- exercíciosGuanabara/ex006.py
+++ exercíciosGuanabara/ex006.py
@@ -1,4 +1,3 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada
 num = float(input('Digite um número: '))
-
 print(f'Número: {num}\nseu dobro: {num*2}\nseu triplo: {num*3}\na raiz quadrada: {num**(1/2)}')
